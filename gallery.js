45683bb8a039acd10c0e97ebf4d54b091f2e1764
Jay <jonnygehlot01@gmail.com>
2023-12-24 00:25:51 +0530

    new

---
+++ gallery.js
@@ -0,0 +1,14 @@
+var box = document.querySelector('.boxX')
+var figures = document.querySelectorAll('.boxX figure')
+
+var cross = document.querySelector('.imaging span')
+cross.addEventListener('click',()=>{
+
+})
+
+box.addEventListener('click', (event) => {
+    console.log(event)
+    figures.forEach((figure), () => {
+        
+    })
+})
