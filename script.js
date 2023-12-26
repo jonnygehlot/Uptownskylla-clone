ec570bd95530b5cf0565813ae420593b33574f14
Jay <jonnygehlot01@gmail.com>
2023-12-26 15:42:47 +0530

    seggrigation in files

---
+++ script.js
@@ -0,0 +1,14 @@
+var ham = document.querySelector('.mplay')
+var sideMenu = document.querySelector('.fixedSlider')
+var sideMenuCross = document.querySelector('.fixedSlider ul li:first-child')
+ham.addEventListener('click', () => {
+    sideMenu.style.left = '0px'
+})
+
+sideMenuCross.addEventListener('click',()=>{
+    sideMenu.style.left = '-100%'
+})
+
+
+
+
--- scripts/script.js
+++ scripts/script.js
@@ -7,8 +7,10 @@ ham.addEventListener('click', () => {
 
 sideMenuCross.addEventListener('click', () => {
     sideMenu.style.left = '-100%'
-})
+});
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>;
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>;
 $(document).ready(function () {
     // add all to same gallery
     $(".boxX a").attr("data-fancybox", "mygallery");
@@ -20,3 +22,18 @@ $(document).ready(function () {
     $(".boxX a").fancybox();
 });
 
+//Gallery
+var box = document.querySelector('.boxX')
+var figures = document.querySelectorAll('.boxX figure')
+
+var cross = document.querySelector('.imaging span')
+cross.addEventListener('click', () => {
+
+})
+
+box.addEventListener('click', (event) => {
+    console.log(event)
+    figures.forEach((figure), () => {
+
+    })
+})
